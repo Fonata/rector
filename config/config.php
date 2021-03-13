537fdd5acef2cc372e4e0a6087b42435acd8eaef
TomasVotruba <tomas.vot@gmail.com>
2021-03-13 22:05:23 +0100

    decouple servies-packages.php config

--- config/config.php
+++ config/config.php
@@ -10,6 +10,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     // @todo this should be removed
     $containerConfigurator->import(__DIR__ . '/services.php');
     $containerConfigurator->import(__DIR__ . '/services-rules.php');
+    $containerConfigurator->import(__DIR__ . '/services-packages.php');
     $containerConfigurator->import(__DIR__ . '/parameters.php');
     $containerConfigurator->import(__DIR__ . '/../utils/*/config/config.php', null, true);
 
