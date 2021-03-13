f43dcf986199491b29dcd7c40280045d942150e9
TomasVotruba <tomas.vot@gmail.com>
2021-03-13 21:49:33 +0100

    use config based tests for rules

--- rules-tests/Symfony/Rector/ClassMethod/NormalizeAutowireMethodNamingRector/NormalizeAutowireMethodNamingRectorTest.php
+++ rules-tests/Symfony/Rector/ClassMethod/NormalizeAutowireMethodNamingRector/NormalizeAutowireMethodNamingRectorTest.php
@@ -27,8 +27,8 @@ final class NormalizeAutowireMethodNamingRectorTest extends AbstractRectorTestCa
         return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
     }
 
-    protected function getRectorClass(): string
+    public function provideConfigFilePath(): string
     {
-        return NormalizeAutowireMethodNamingRector::class;
+        return __DIR__ . '/config/configured_rule.php';
     }
 }
